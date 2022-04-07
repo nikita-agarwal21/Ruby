e87868d911fc82e57c9b432303e8af2b1b04bbf2
nikita-agarwal21 <81410759+nikita-agarwal21@users.noreply.github.com>
2022-04-07 23:19:46 +0530

    Create section3.rb

---
+++ section3.rb
@@ -0,0 +1,29 @@
+● Program - Check if a given string is Palindrome
+
+def palindrome?(string)
+irb(main):016:1> if string == string.reverse
+irb(main):017:2>   puts "palindrome"
+irb(main):018:2> else 
+irb(main):019:2* puts "not palindrome"
+irb(main):020:2> end
+irb(main):021:1> end
+=> :palindrome?
+---------------------------------------------------------
+
+● Program - Compare elements between 2 different arrays
+
+● Program - Linear search
+● Program - Binary search
+● Program - Find the Largest Element of an Array
+● Program - Find the Smallest Element of an Array
+● Program - Add Two Matrix Using Multi-dimensional Arrays
+● Program - Multiply to Matrix Using Multi-dimensional Arrays
+● Program - Find Transpose of a Matrix
+● Program - Calculate Average Using Arrays
+● Program - Check if An Array Contains a Given Value
+● Program - Merge Two Arrays
+● Program - Check if Two Arrays Are Equal or Not
+● Program - Remove All Occurrences of an Element in an Array
+● Program - Find Common Array Elements
+● Program - Copy All the Elements of One Array to Another Array
+● Program - Array Rotation
